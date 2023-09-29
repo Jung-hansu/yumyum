91dc96fd4285ac27ac7ffbb838635785a7ab9dc7
정한수 <jounghs3933@gmail.com>
2023-09-29 22:33:29 +0900

    한수-users app 추가

---
+++ yumyum-backend/users/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def createusers(request):
+    return HttpResponse("추후 개발 예정")
+
+def login(request):
+    return HttpResponse("로그인 기능 추후 개발 예정")
+
+def logout(request):
+    return HttpResponse("추후 개발 예정")
