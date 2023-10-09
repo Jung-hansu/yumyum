6aa40234173d5d68de05d7dcaeb4d12799492636
정한수 <jounghs3933@gmail.com>
2023-10-10 02:47:33 +0900

    index 및 templates 생성

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from users.views import *
 
 urlpatterns = [
+    path('', index),
     path('login/', login),
     path('logout/', logout),
     path('signup/', signup),
--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect, HttpResponse
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
+from .models import User
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
 # Create your views here.
+def index(request):
+    return render(request, 'users/index.html')
+
 def signup(request):
     return HttpResponse("추후 개발 예정")
 
@@ -14,6 +17,12 @@ def login(request):
         if user is not None:
             auth_login(request, user)
     return render(request, 'users/login.html')
+
+def authenticate(id, pw):
+    for users in User.objects.all():
+        if users.id == id and users.pw == pw:
+            return users
+    return None
 
 def logout(request):
     auth_logout(request)
