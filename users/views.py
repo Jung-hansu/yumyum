86762e4529fd7d9725742f22bd0563ebbde36ca0
정한수 <jounghs3933@gmail.com>
2023-10-10 19:20:25 +0900

    forms.py 생성

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from .models import User
+from .forms import UserForm
 from datetime import datetime
 
 # Create your views here.
@@ -8,18 +9,26 @@ def index(request):
     return render(request, 'users/index.html')
 
 def signup(request):
-    # 이미 존재하는 유저인지 파악
-    # 전화번호가 일치하거나 id & pw가 일치하는 경우 존재하는 유저로 취급
-    is_exist = False
-    if request.method == 'POST':
-        for user in User.objects.all():
-            if  user.phone_number == request.POST['phone_number'] or \
-                (user.id == request.POST['id'] and user.pw == request.POST['pw']):
-                is_exist = True
-                break
-        if not is_exist:
-            return add_user(request)
-    return render(request, 'users/signup.html', {'is_exist':is_exist})
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            new_user = User.objects.create_user(**form.cleaned_data)
+            login(request, new_user)
+            return redirect('index')
+    else:
+        form = UserForm()
+        return render(request, 'memo_app/adduser.html', {'form': form})
+    
+    # is_exist = False
+    # if request.method == 'POST':
+    #     for user in User.objects.all():
+    #         if  user.phone_number == request.POST['phone_number'] or \
+    #             (user.id == request.POST['id'] and user.pw == request.POST['pw']):
+    #             is_exist = True
+    #             break
+    #     if not is_exist:
+    #         return add_user(request)
+    # return render(request, 'users/signup.html', {'is_exist':is_exist})
 
 def add_user(request):
     if request.method == 'POST':
@@ -42,16 +51,16 @@ def login(request):
         if user is not None:
             # 페이크 로그인임. 세션 기능 추가해야함
             auth_login(request, user)
-            # return redirect('/users', {'is_authenticated':True, 'user':user})
+            # return redirect('users:login')
             return render(request, 'users/login.html', {'is_authenticated':True, 'user':user})
         wrong_input = True
     return render(request, 'users/login.html', {'wrong_input':wrong_input})
 
-def my_authenticate(id, pw):
-    for user in User.objects.all():
-        if user.id == id and user.pw == pw:
-            return user
-    return None
+# def my_authenticate(id, pw):
+#     for user in User.objects.all():
+#         if user.id == id and user.pw == pw:
+#             return user
+#     return None
 
 def logout(request):
     auth_logout(request)
