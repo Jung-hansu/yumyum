524c4e636fa2a2a2c62526f2dc4e1c99b82fee86
wisenick1 <51376082+wisenick1@users.noreply.github.com>
2023-12-10 13:41:49 +0900

    move review to restaurant, user

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -2,13 +2,14 @@ from django.urls import path, include
 from restaurants.views import *
 
 urlpatterns = [
-    path('', CreateRestaurantView.as_view()),                               # 관리자용 식당 추가 기능
-    path('<int:restaurant_id>/', RestaurantInfoView.as_view()),             # 식당 조회
-    path('filtered/', RestaurantFilterView.as_view()),                      # 필터링
-    path('<int:restaurant_id>/waitings/', RestaurantWaitingView.as_view()), # 예약
-    path('manage/', RestaurantManagerView.as_view()),                       # 식당 매니저
-    path('manage/<int:restaurant_id>/', RestaurantManagementView.as_view()),# 식당 관리
-    path('<int:restaurant_id>/reviews/', include('reviews.urls')),          # 리뷰로 연결
+    path('', CreateRestaurantView.as_view()),                                 # 관리자용 식당 추가 기능
+    path('<int:restaurant_id>/', RestaurantInfoView.as_view()),               # 식당 조회
+    path('filtered/', RestaurantFilterView.as_view()),                        # 필터링
+    path('<int:restaurant_id>/waitings/', RestaurantWaitingView.as_view()),   # 예약
+    path('manage/', RestaurantManagerView.as_view()),                         # 식당 매니저
+    path('manage/<int:restaurant_id>/', RestaurantManagementView.as_view()),  # 식당 관리
+    path('<int:restaurant_id>/reviews/', RestaurantReviewListView.as_view()), # 식당 리뷰 조회
+    path('<int:restaurant_id>/reviews/write/', WriteReivew.as_view()),        # 리뷰 남기기
 ]
 
 
